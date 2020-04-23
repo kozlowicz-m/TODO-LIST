49baaa9e2a6ef7bca34c6ebfca92375af2f81eaf
Marta Kozlowicz <Marta.Kozlowicz@stepstone.com>
2020-04-23 13:40:02 +0200

    Some changes in webpack

--- TODO-LIST/webpack.config.js
+++ TODO-LIST/webpack.config.js
@@ -1,12 +1,24 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require("path");
 
 module.exports = {
-    entry: "./scripts/components/ToDoList.tsx",
+    entry: "./index.tsx",
     output: {
         filename: "bundle.min.js",
-        path: path.resolve(__dirname, "./dist")
+        path: path.resolve(__dirname, "dist")
     },
+    plugins: [new HtmlWebpackPlugin()],
     watch: false,
     mode: "development",
     devtool: "source-map",
+    resolve: {
+        // Add `.ts` and `.tsx` as a resolvable extension.
+        extensions: [".ts", ".tsx", ".js"]
+    },
+    module: {
+        rules: [
+            // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+            { test: /\.tsx?$/, loader: "ts-loader" }
+        ]
+    }
 }
